ffc2cf70df699975f51ee25fabb5fd769110ac9d
DevDobariya75 <devdobariya75@gmail.com>
2026-03-23 11:01:47 +0530

    Implement initial server setup with Express, CORS, and MongoDB connection; add API error handling utilities

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,27 @@
-console.log("Hello, World!");
-console.log("this is dev dobariya");
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import { app } from "./app.js";
+import connectDB from './db/index.js';
+
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!" , err);
+
+})
+
+
+
+
+
+
